a42e08a2b4fab16851632fb4c1ae8713723112ea
Melwin Amith D'Almeida <melwinalm@gmail.com>
2021-04-11 15:20:50 +0530

    Adds on complete functionality

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function TaskItem({ id, taskName, onComplete, onDelete }) {
+function TaskItem({ id, taskName, taskStatus, onComplete, onDelete }) {
   return (
     <div class="mt-3 mb-3">
-      <input type="checkbox" class="mr-3" />
+      <input
+        checked={taskStatus}
+        type="checkbox"
+        class="mr-3"
+        onChange={(e) => onComplete(id, e.target.checked)}
+      />
       <span class="subtitle">{taskName}</span>
       <span class="is-pulled-right">
         <button class="delete" onClick={() => onDelete(id)}></button>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -10,7 +10,10 @@ function TaskList({ localKey }) {
     if (localStorage.getItem(localKey)) {
       setList(JSON.parse(localStorage.getItem(localKey)));
     } else {
-      let tempTasks = [{ name: "Task 1" }, { name: "Task 2" }];
+      let tempTasks = [
+        { name: "Task 1", isComplete: false },
+        { name: "Task 2", isComplete: false }
+      ];
       setList(tempTasks);
       localStorage.setItem(localKey, JSON.stringify(tempTasks));
     }
@@ -22,6 +25,17 @@ function TaskList({ localKey }) {
 
   const OnTaskDeletion = (id) => {
     setList(list.filter((val, index) => index !== id));
+  };
+
+  const OnTaskCompletion = (id, flag) => {
+    setList(
+      list.map((val, index) => {
+        if (index === id) {
+          val.isComplete = flag;
+        }
+        return val;
+      })
+    );
   };
 
   const AddNewItem = () => {
@@ -49,12 +63,13 @@ function TaskList({ localKey }) {
             <div class="has-text-centered m-4">No tasks found</div>
           )}
         </div>
-        <div>
+        <div style={{ overflowY: "auto" }}>
           {list.map((val, i) => (
             <TaskItem
               id={i}
               taskName={val.name}
-              onComplete=""
+              taskStatus={val.isComplete}
+              onComplete={(id, flag) => OnTaskCompletion(id, flag)}
               onDelete={(id) => OnTaskDeletion(id)}
             />
           ))}
